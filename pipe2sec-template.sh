b8b44d6cb83382b32496cba1f145fec6af6d8ddf
Keegan Bowen <keegan.bowen@gmail.com>
2019-09-28 04:22:01 -0600

    Create pipe2sec-template.sh

---
+++ pipe2sec-template.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# edit the file to populate for your situation
+
+# someuser is the user that has the authorized public key
+# central_host is a centralized logging location to send the data to
+# someplace is the directory structure you need
+# some_central_log.log is the centralized log for the dataset at hand
+# local_log.log is the local log that is getting appended to the central log
+
+
+while true; do
+        ssh someuser@central_host 'cat >> /someplace/some_central_log.log' < /someplace/local_log.log
+done
